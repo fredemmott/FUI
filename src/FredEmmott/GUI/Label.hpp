ef5a3fd0ec425ba29d070e34b659c97d37536f10
Fred Emmott <git@fredemmott.com>
2024-12-22 06:48:56 -0600

    Better handling of format strings

--- src/FredEmmott/GUI/Label.hpp
+++ src/FredEmmott/GUI/Label.hpp
@@ -4,30 +4,32 @@
 
 #include <format>
 
-#include "Widget.hpp"
-#include "Font.hpp"
 #include "Color.hpp"
+#include "Font.hpp"
+#include "Widget.hpp"
 
 namespace FredEmmott::GUI {
 
 class Label final : public Widget {
  public:
   struct Options {
-    Font mFont { SystemFont::Usage::Body };
-    Color mColor { SystemColor::Usage::Foreground };
+    Font mFont {SystemFont::Usage::Body};
+    Color mColor {SystemColor::Usage::Foreground};
   };
 
   template <class... Args>
-  explicit Label(const Options& options, std::format_string<Args...> format, Args&&... args)
-    : Label(internal_t {}, options, format, std::format(format, std::forward<Args>(args)...)) {
+  explicit Label(
+    const Options& options,
+    std::format_string<Args...> format,
+    Args&&... args)
+    : Label(options, FormattedLabel {format, std::forward<Args>(args)...}) {
   }
 
   void Paint(SkCanvas*) const override;
 
  private:
-  struct internal_t {};
-  Label(internal_t, const Options& options, auto format, const std::string_view formatted)
-    : Widget(FormattedToId(format, formatted)), mOptions(options), mText(formatted) {
+  Label(const Options& options, const FormattedLabel& label)
+    : Widget(label.mID), mOptions(options), mText(label.mText) {
     this->SetLayoutConstraints();
   }
 
