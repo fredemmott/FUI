4a8f4d0b1a8ac83a29af482aba41362d57c3b15a
Fred Emmott <git@fredemmott.com>
2024-12-25 10:25:07 -0600

    🔥 Delete unused single_child_widget and multi_child_widget concepts

--- src/FredEmmott/GUI/Immediate/concepts.hpp
+++ src/FredEmmott/GUI/Immediate/concepts.hpp
@@ -16,20 +16,4 @@ concept string_widget = widget<T> && requires(T widget) {
   { widget.GetText() } -> std::convertible_to<std::string_view>;
 };
 
-template <class T>
-concept single_child_widget
-  = widget<T> && requires(T widget, Widgets::Widget* child) {
-      widget.SetChild(child);
-      { widget.GetChild() } -> std::convertible_to<Widgets::Widget*>;
-    };
-
-template <class T>
-concept multi_child_widget
-  = widget<T> && requires(T widget, std::vector<Widgets::Widget*> children) {
-      widget.AppendChild(children.front());
-      widget.SetChildren(std::move(children));
-      { widget.GetChildren() } -> std::ranges::input_range;
-      { *begin(widget.GetChildren()) } -> std::convertible_to<Widgets::Widget*>;
-    };
-
 }// namespace FredEmmott::GUI::Immediate
