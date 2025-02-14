e53795092a1b19ed3b9b8ac9ccf81ec85e657650
Fred Emmott <git@fredemmott.com>
2025-02-14 16:42:08 -0600

    🐛 Fix up include for yoga.cpp

--- src/FredEmmott/GUI/yoga.cpp
+++ src/FredEmmott/GUI/yoga.cpp
@@ -1,9 +1,9 @@
 // Copyright 2025 Fred Emmott <fred@fredemmott.com>
 // SPDX-License-Identifier: MIT
 
-#include <thread>
-
 #include "yoga.hpp"
+
+#include <mutex>
 
 namespace FredEmmott::GUI {
 YGConfigRef GetYogaConfig() {
@@ -16,4 +16,4 @@ YGConfigRef GetYogaConfig() {
   });
   return sInstance.get();
 }
-}
+}// namespace FredEmmott::GUI
