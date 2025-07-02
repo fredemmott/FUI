8c68b0ce0fc4e8565754c355a17a2bea05961fe2
Fred Emmott <git@fredemmott.com>
2025-07-02 10:29:05 -0500

    ♻️ Move most of the conditional Result stuff into Mixins

--- src/FredEmmott/GUI/Immediate/Result.hpp
+++ src/FredEmmott/GUI/Immediate/Result.hpp
@@ -1,4 +1,4 @@
-// Copyright 2025 Fred Emmott <fred@fredemmott.com>
+// Copyright 2025 Fred Emmott<fred @fredemmott.com>
 // SPDX-License-Identifier: MIT
 #pragma once
 
@@ -28,39 +28,83 @@ auto widget_from_result(const T& v) {
   return widget_from_result_t<T> {}(v);
 }
 
+template <ResultOptions TOptions>
+struct StyledResultMixin {};
+
+template <ResultOptions TOptions>
+  requires(TOptions.mHasWidgetPointer)
+struct StyledResultMixin<TOptions> {
+  template <class Self>
+  decltype(auto) Styled(this Self&& self, const Style& style) {
+    widget_from_result(self)->ReplaceExplicitStyles(style);
+    return std::forward<Self>(self);
+  }
+};
+
+template <void (*TEndWidget)()>
+struct ScopedEndWidget {
+  ScopedEndWidget(const ScopedEndWidget&) = delete;
+  ScopedEndWidget& operator=(const ScopedEndWidget&) = delete;
+
+  constexpr ScopedEndWidget() = default;
+  constexpr ScopedEndWidget(ScopedEndWidget&&) noexcept = default;
+  constexpr ScopedEndWidget& operator=(ScopedEndWidget&&) noexcept = default;
+
+  constexpr ~ScopedEndWidget() {
+    if (!mMoved) {
+      TEndWidget();
+    }
+  }
+
+ private:
+  utility::moved_flag mMoved;
+};
+
+template <void (*TEndWidget)()>
+struct ScopedResultMixin {
+  auto Scoped() {
+    if (std::exchange(mScoped, true)) [[unlikely]] {
+      throw std::logic_error("Can't call Scoped() twice on the same Result");
+    }
+    return ScopedEndWidget<TEndWidget> {};
+  }
+
+ private:
+  bool mScoped = false;
+};
+template <>
+struct ScopedResultMixin<nullptr> {};
+
+template <class TValue>
+struct ValueResultMixin {
+  constexpr TValue GetValue(this const auto& self) noexcept {
+    return self.mValue;
+  }
+
+  constexpr operator TValue(this const auto& self) noexcept {
+    return self.GetValue();
+  }
+};
+template <>
+struct ValueResultMixin<void> {};
+
 }// namespace FredEmmott::GUI::Immediate::immediate_detail
 
 namespace FredEmmott::GUI::Immediate {
-
 template <
   void (*TEndWidget)() = nullptr,
-  class TReturn = void,
+  class TValue = void,
   immediate_detail::ResultOptions TOptions = {}>
-class Result final {
+class Result final : public immediate_detail::StyledResultMixin<TOptions>,
+                     public immediate_detail::ScopedResultMixin<TEndWidget>,
+                     public immediate_detail::ValueResultMixin<TValue> {
  public:
   static constexpr bool HasWidget = TOptions.mHasWidgetPointer;
-  static constexpr bool HasValue = !std::is_void_v<TReturn>;
-
-  struct ScopedEndWidget {
-    ScopedEndWidget(const ScopedEndWidget&) = delete;
-    ScopedEndWidget& operator=(const ScopedEndWidget&) = delete;
-
-    constexpr ScopedEndWidget() = default;
-    constexpr ScopedEndWidget(ScopedEndWidget&&) noexcept = default;
-    constexpr ScopedEndWidget& operator=(ScopedEndWidget&&) noexcept = default;
-
-    constexpr ~ScopedEndWidget() {
-      if (!mMoved) {
-        TEndWidget();
-      }
-    }
-
-   private:
-    utility::moved_flag mMoved;
-  };
+  static constexpr bool HasValue = !std::is_void_v<TValue>;
 
   template <void (*)(), class, immediate_detail::ResultOptions>
   friend class Result;
+  friend struct immediate_detail::ValueResultMixin<TValue>;
   friend std::conditional_t<
     HasWidget,
     immediate_detail::widget_from_result_t<Result>,
@@ -72,60 +116,35 @@ class Result final {
     requires(!HasValue && !HasWidget)
   = default;
 
+  template <std::convertible_to<TValue> T>
+    requires(HasValue && !HasWidget)
+  constexpr Result(T&& result) : mValue(std::forward<T>(result)) {}
+
   constexpr Result(Widgets::Widget* widget)
     requires(HasWidget && !HasValue)
     : mWidget(widget) {};
-
-  template <std::convertible_to<TReturn> T>
-    requires(HasWidget && HasValue)
-  constexpr Result(Widgets::Widget* widget, T&& result)
-    : mWidget(widget),
-      mResult(std::forward<T>(result)) {}
-
-  template <std::convertible_to<TReturn> T>
-    requires(HasValue && !HasWidget)
-  constexpr Result(T&& result) : mResult(std::forward<T>(result)) {}
 
   template <void (*TOtherEndWidget)()>
     requires(HasWidget && !HasValue)
   constexpr Result(const Result<TOtherEndWidget, void>& other)
     : mWidget(other.mWidget) {}
 
-  template <std::convertible_to<TReturn> T, void (*TOtherEndWidget)()>
+  template <std::convertible_to<TValue> T>
+    requires(HasWidget && HasValue)
+  constexpr Result(Widgets::Widget* widget, T&& result)
+    : mWidget(widget),
+      mValue(std::forward<T>(result)) {}
+
+  template <std::convertible_to<TValue> T, void (*TOtherEndWidget)()>
     requires(HasWidget && HasValue)
   constexpr Result(const Result<TOtherEndWidget, void>& other, T&& result)
     : mWidget(other.mWidget),
-      mResult(std::forward<T>(result)) {}
-
-  constexpr TReturn GetValue() const noexcept
-    requires(HasValue)
-  {
-    return mResult;
-  }
-
-  constexpr operator TReturn() const noexcept
-    requires(HasValue)
-  {
-    return GetValue();
-  }
-
-  auto& Styled(const Style& style)
-    requires HasWidget
-  {
-    mWidget->ReplaceExplicitStyles(style);
-    return *this;
-  }
-
-  constexpr static auto Scoped()
-    requires(TEndWidget != nullptr)
-  {
-    return ScopedEndWidget {};
-  }
+      mValue(std::forward<T>(result)) {}
 
  private:
   FUI_NO_UNIQUE_ADDRESS
   std::conditional_t<HasWidget, Widgets::Widget*, std::monostate> mWidget {};
   FUI_NO_UNIQUE_ADDRESS
-  std::conditional_t<HasValue, TReturn, std::monostate> mResult;
+  std::conditional_t<HasValue, TValue, std::monostate> mValue;
 };
 }// namespace FredEmmott::GUI::Immediate
