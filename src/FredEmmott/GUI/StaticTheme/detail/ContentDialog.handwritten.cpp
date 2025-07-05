808bf01ea82e982bfea919c51eec90e1e74e4d98
Fred Emmott <git@fredemmott.com>
2025-07-05 16:18:03 -0500

    🚧 Add ContentDialog theme files
    refs #44

---
+++ src/FredEmmott/GUI/StaticTheme/detail/ContentDialog.handwritten.cpp
@@ -0,0 +1,25 @@
+// Copyright 2025 Fred Emmott <fred@fredemmott.com>
+// SPDX-License-Identifier: MIT
+
+#include "Contentdialog.handwritten.hpp"
+
+#include <FredEmmott/GUI/PseudoClasses.hpp>
+#include <FredEmmott/GUI/StaticTheme/ContentDialog.hpp>
+#include <FredEmmott/GUI/Style.hpp>
+
+namespace FredEmmott::GUI::StaticTheme::ContentDialog {
+using namespace StaticTheme::Common;
+using namespace StaticTheme::ContentDialog;
+using namespace PseudoClasses;
+
+const style_detail::lazy_init_style DefaultButtonStyle {[] {
+  return Style {
+    .mBackgroundColor = ContentDialogBackground,
+    .mBorderColor = ContentDialogBorderBrush,
+    .mBorderRadius = OverlayCornerRadius,
+    .mBorderWidth = ContentDialogBorderWidth,
+    .mColor = ContentDialogForeground,
+  };
+}};
+
+}// namespace FredEmmott::GUI::StaticTheme::ContentDialog
---
+++ src/FredEmmott/GUI/StaticTheme/detail/ContentDialog.handwritten.hpp
@@ -0,0 +1,11 @@
+// Copyright 2025 Fred Emmott <fred@fredemmott.com>
+// SPDX-License-Identifier: MIT
+#pragma once
+
+#include <FredEmmott/GUI/detail/style/lazy_init_style.hpp>
+
+namespace FredEmmott::GUI::StaticTheme::Dialog {
+
+extern const style_detail::lazy_init_style DefaultContentDialogStyle;
+
+}// namespace FredEmmott::GUI::StaticTheme::Dialog
