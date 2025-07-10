b9377d816f59e9aeb99177ba8e25011ecfedb86a
Fred Emmott <git@fredemmott.com>
2025-07-10 14:03:12 -0500

    ✨ Allow various radio button key types
    in particular, allow enums

--- src/FredEmmott/GUI/Immediate/RadioButton.hpp
+++ src/FredEmmott/GUI/Immediate/RadioButton.hpp
@@ -3,6 +3,7 @@
 #pragma once
 
 #include <concepts>
+#include <type_traits>
 
 #include "FredEmmott/GUI/detail/immediate_detail.hpp"
 #include "ID.hpp"
@@ -10,13 +11,29 @@
 
 namespace FredEmmott::GUI::Immediate {
 
+/** An identifier for the selected item in a set of radio buttons.
+ *
+ * This is primarily intended to allow ints, `std::size_t`, and
+ * enums, but given all we need to allow is equality, it's fairly
+ * permissive.
+ *
+ * - Pointers are disallowed to avoid confusion for parameters that
+ *   take a pointer
+ * - floats are disallowed as while they support ==, using == is
+ *   usually a bad idea
+ */
+template <class T>
+concept radio_button_key = std::equality_comparable<T>
+  && std::is_trivially_copyable_v<T> && (!std::is_pointer_v<T>)
+  && (!std::is_void_v<T>) && (!std::is_floating_point_v<T>);
+
 void EndRadioButton();
 [[nodiscard]]
 Result<&EndRadioButton, bool> BeginRadioButton(
   bool isInitiallyChecked,
   ID id = ID {std::source_location::current()});
 
-template <std::unsigned_integral T>
+template <radio_button_key T>
 Result<&EndRadioButton, bool> BeginRadioButton(
   T* checkedIndex,
   const T index,
@@ -35,7 +52,7 @@ Result<nullptr, bool> RadioButton(
   std::string_view label,
   ID = ID {std::source_location::current()});
 
-template <std::unsigned_integral T>
+template <radio_button_key T>
 Result<nullptr, bool> RadioButton(
   T* checkedIndex,
   const T index,
@@ -59,8 +76,8 @@ Result<nullptr, bool> RadioButton(
   return RadioButton(isInitiallyChecked, text, id);
 }
 
-template <std::unsigned_integral T, class... Args>
-  requires((!std::same_as<ID, std::decay_t<Args>>) && ...)
+template <radio_button_key T, class... Args>
+  && ((!std::same_as<ID, std::decay_t<Args>>) && ...)
 Result<nullptr, bool> RadioButton(
   T* checkedIndex,
   const T index,
