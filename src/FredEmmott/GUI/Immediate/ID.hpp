85ed9ca5044ca7298cb01a3d3814554682cc6033
Fred Emmott <git@fredemmott.com>
2026-02-26 11:02:54 -0600

    🐛 Fix ID collision if multiple source files are used in the same scope and on the same line/column
    - Use FNV-1A on the filename, line, and column
    - As we've now get FNV-1A - and it's constexpr - also use it for strings

--- src/FredEmmott/GUI/Immediate/ID.hpp
+++ src/FredEmmott/GUI/Immediate/ID.hpp
@@ -2,6 +2,8 @@
 // SPDX-License-Identifier: MIT
 #pragma once
 
+#include <array>
+#include <bit>
 #include <format>
 #include <source_location>
 #include <string>
@@ -26,14 +28,18 @@ class ID final {
 
   explicit constexpr ID(const std::size_t id) : mID(id) {}
 
-  explicit constexpr ID(const std::source_location& location)
-    : mID((location.line() << 8) | (location.column() & 0xff)) {}
+  explicit constexpr ID(const std::source_location& location);
+
+  explicit constexpr ID(const std::string_view string) : mID(Hash(string)) {}
 
   template <class... TArgs>
     requires(sizeof...(TArgs) > 0)
-  explicit constexpr ID(std::format_string<TArgs...> fmt, TArgs&&... args) {
-    mID = std::hash<std::string_view> {}(
-      std::format(fmt, std::forward<TArgs>(args)...));
+  explicit
+#ifdef __cpp_lib_constexpr_format// P3391
+    constexpr
+#endif
+    ID(std::format_string<TArgs...> fmt, TArgs&&... args) {
+    mID = Hash(std::format(fmt, std::forward<TArgs>(args)...));
   }
 
   [[nodiscard]]
@@ -42,6 +48,54 @@ class ID final {
   }
 
  private:
-  std::size_t mID;
+  std::size_t mID {};
+  constexpr std::size_t static HashStep(
+    const std::size_t hash,
+    const uint8_t byte) {
+    return (hash ^ byte) * FNVPrime();
+  }
+
+  template <std::integral T>
+    requires(!std::same_as<uint8_t, T>)
+  constexpr std::size_t static HashMixin(std::size_t hash, const T value) {
+    for (auto&& byte: std::bit_cast<std::array<uint8_t, sizeof(T)>>(value)) {
+      hash = HashStep(hash, byte);
+    }
+    return hash;
+  }
+
+  // ReSharper disable once CppDFAConstantFunctionResult
+  static constexpr std::size_t FNVPrime() {
+    if constexpr (sizeof(std::size_t) == 8) {
+      return 0x00000100000001b3;
+    } else if constexpr (sizeof(std::size_t) == 4) {
+      return 0x01000193;
+    }
+  }
+
+  // ReSharper disable once CppDFAConstantFunctionResult
+  static constexpr std::size_t FNVOffsetBasis() {
+    if constexpr (sizeof(std::size_t) == 8) {
+      return 0xcbf29ce484222325;
+    } else if constexpr (sizeof(std::size_t) == 4) {
+      return 0x811c9dc5;
+    }
+  }
+
+  /// FNV-1a
+  static constexpr std::size_t Hash(const std::string_view str) {
+    std::size_t hash = FNVOffsetBasis();
+    for (const auto byte: str) {
+      hash = HashStep(hash, static_cast<uint8_t>(byte));
+    }
+    return hash;
+  }
 };
+
+constexpr ID::ID(const std::source_location& location) {
+  mID = Hash(location.file_name());
+  mID = HashMixin(mID, location.line());
+  mID = HashMixin(mID, location.column());
+}
+
 };// namespace FredEmmott::GUI::Immediate
