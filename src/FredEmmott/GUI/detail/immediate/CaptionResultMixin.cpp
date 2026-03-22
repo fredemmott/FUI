2db9f6c94a39bf63cd6b37af711b39d332b59fc5
Fred Emmott <git@fredemmott.com>
2026-03-22 18:18:41 -0500

    ⚡ In `.Caption(...)`, search backwards for target widget
    It will usually be the last but one, but will always be near the end.

--- src/FredEmmott/GUI/detail/immediate/CaptionResultMixin.cpp
+++ src/FredEmmott/GUI/detail/immediate/CaptionResultMixin.cpp
@@ -23,8 +23,15 @@ void CaptionResultMixin::AttachCaption(
 
   auto& siblings = tStack.back().mNewSiblings;
   FUI_ASSERT(siblings.back() == caption);
-  const auto it = std::ranges::find(siblings, target);
-  FUI_ASSERT(it != siblings.end());
+  // - search backwards as the widget will usually be the previous sibling, but
+  //   will always be near the end
+  // - skip 1 as the last is the caption widget
+  const auto rit = std::ranges::find(
+    std::ranges::next(siblings.rbegin()), siblings.rend(), target);
+  FUI_ASSERT(rit != siblings.rend());
+  // an iterator points at the gap before an element
+  // a reverse iterator points at the gap after an element
+  const auto it = std::ranges::prev(rit.base());
   std::ranges::rotate(it, siblings.end() - 1, siblings.end());
 }
 
